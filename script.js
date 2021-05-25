af03b90d529b7be245b0c9c38aae88836c29a0bf
David Sanchez <davidmsanchez250@gmail.com>
2021-05-24 21:27:56 -0400

    created for loop to create forecast for next 5 days. Changed current forecasted data to take in OneCall parameter rather than returned data.

--- script.js
+++ script.js
@@ -1,61 +1,145 @@
 const returnedDataDiv = document.querySelector('#returnedDataDiv')
-const searchedCity = document.querySelector('#searchedCity')
+const fiveDayForecastDiv = document.querySelector('#fiveDayForecast')
+// const searchedCity = document.querySelector('#searchedCity')
 const searchForm = document.querySelector('#searchCityForm');
+const apiKey = '3664890572785f16ad10031c24428df2'
+
 
 function handleFormSubmit (event) {
     event.preventDefault();
-
-
-
-currentWeather(event);
-
-};
-
-function currentWeather (e) {
-    e.preventDefault();
-
     const searchedCityVal = document.querySelector('#city').value;
-
     if (!searchedCityVal) {
         console.error('please input city')
         return;
     }
     console.log(searchedCityVal)
 
-    let resultCard = document.createElement('div');
-    resultCard.classList.add('card', 'bg-dark', 'text-white', 'mb-3', 'p-3');
+currentWeather(searchedCityVal);
 
-    let titleEl = document.createElement('div');
-    titleEl.classList.add('card-header', 'bg-dark', 'text-white');
-    titleEl.textContent = searchedCityVal + '    ' + 'date';
-    resultCard.append(titleEl)
+};
 
-    let resultBody = document.createElement('div');
-    resultBody.classList.add('card-body');
-
-    let tempEl = document.createElement('p');
-    tempEl.innerText = 'Temp:';
-
-    let windEl = document.createElement('p');
-    windEl.innerText = 'Wind:';
-
-    let humidityEl = document.createElement('p');
-    humidityEl.innerText = 'Humidity:';
-
-    resultBody.append(tempEl, windEl, humidityEl);
-
-    resultCard.append(resultBody);
+function currentWeather (searchedCityVal) {
 
 
-    searchedCity.append(resultCard)
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchedCityVal + '&appid=' + apiKey + '&units=imperial'
+
+    fetch(apiUrl)
+    .then(function (response){
+        if (!response.ok) {
+            throw response.json();
+          }
+    
+          return response.json();
+        
+    })
+    .then(function (returnedData){
+        console.log(returnedData)
+        createForecast(returnedData.coord.lat, returnedData.coord.lon)
+    })
+
 
 
 }
 
-function searchWeatherData (e) {
-    // const lat = 
-    // const lon = 
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=Trilogy'
+function forecastedFiveDay (e) {
+    e.preventDefault();
+
+
+}
+
+function createForecast (lat, lon) {
+
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial'
+
+    fetch(apiUrl)
+    .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+  
+        return response.json();
+      })
+    .then(function(oneCallData){
+        console.log(oneCallData)
+        //for loop for creating cards 
+        //i less than 5 
+
+        let returnedDataCard = document.createElement('div');
+        returnedDataCard.classList.add('card', 'bg-dark', 'text-white', 'mb-3', 'p-3');
+    
+        let titleEl = document.createElement('div');
+        titleEl.classList.add('card-header', 'bg-dark', 'text-white');
+        titleEl.textContent = 'Current Forecast for ' + document.querySelector('#city').value;
+        returnedDataCard.append(titleEl)
+
+        let weatherEl = document.createElement('p');
+        weatherEl.innerText = 'General Weather Conditions: ' + oneCallData.current.weather[0].description
+        
+        let tempEl = document.createElement('p');
+        tempEl.innerText = 'Avg Temp: ' + oneCallData.current.temp;
+    
+        let feelsLikeEl = document.createElement('p');
+        feelsLikeEl.innerText = 'Feels Like:' + ' ' + oneCallData.current.feels_like;
+    
+        let humidityEl = document.createElement('p');
+        humidityEl.innerText = 'Humidity:' + ' ' + oneCallData.current.humidity;
+
+        let windSpeedEl = document.createElement('p');
+        windSpeedEl.innerText = 'Wind Speed: ' + oneCallData.current.wind_speed
+
+        let uvIndexEl = document.createElement('p');
+        uvIndexEl.innerText = 'UV Index: ' + oneCallData.current.uvi
+
+        let resultBody = document.createElement('div');
+        resultBody.classList.add('card-body');
+    
+        resultBody.append(weatherEl, tempEl, feelsLikeEl, humidityEl, windSpeedEl, uvIndexEl);
+    
+        returnedDataCard.append(resultBody);
+    
+    
+        returnedDataDiv.append(returnedDataCard)
+
+
+        for (let index = 0; index < 5; index++) {
+            
+            let returnedDataCard = document.createElement('div');
+            returnedDataCard.classList.add('card', 'bg-dark', 'text-white', 'mb-3', 'p-3');
+
+            let weatherEl = document.createElement('p');
+            weatherEl.innerText = 'General Weather Conditions: ' + oneCallData.daily[index].weather[0].description;    
+    
+            let highTempEl = document.createElement('p');
+            highTempEl.innerText = 'High of: ' + oneCallData.daily[index].temp.max;
+    
+            let lowTempEl = document.createElement('p');
+            lowTempEl.innerText = 'Low of: ' + oneCallData.daily[index].temp.min;
+            
+            let feelsLikeEl = document.createElement('p');
+            feelsLikeEl.innerText = 'Feels Like:' + ' ' + oneCallData.daily[index].feels_like.day;
+        
+            let humidityEl = document.createElement('p');
+            humidityEl.innerText = 'Humidity:' + ' ' + oneCallData.daily[index].humidity;
+
+            let windSpeedEl = document.createElement('p');
+            windSpeedEl.innerText = 'Wind Speed: ' + oneCallData.daily[index].wind_speed
+
+            let uvIndexEl = document.createElement('p');
+            uvIndexEl.innerText = 'UV Index: ' + oneCallData.daily[index].uvi
+    
+            let resultBody = document.createElement('div');
+            resultBody.classList.add('card-body');
+        
+            resultBody.append(weatherEl, highTempEl, lowTempEl, feelsLikeEl, humidityEl, windSpeedEl, uvIndexEl);
+        
+            returnedDataCard.append(resultBody);
+        
+        
+            fiveDayForecastDiv.append(returnedDataCard)
+        
+    
+        }
+    })
 }
 
 searchForm.addEventListener('submit', handleFormSubmit);
